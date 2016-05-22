ee8ad22c7bf1d30d35bdab4cb4a34c6125239b1f
zmx@.1 <hksuppots@163.com>
2016-05-22 19:49:32 +0800

    1

--- sdd/src/main/java/com/zmx/dao/HomeDao.java
+++
@@ -1,9 +0,0 @@
-package com.zmx.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.zmx.domain.THome;
-
-public interface HomeDao extends CrudRepository<THome, Integer>{
-
-}
